d5583659cd40bc138122d4046dfca55bea2932d2
Chris Tasatzis <christasa@hotmail.com>
2020-06-05 17:45:54 +0300

    added docker compose

---
+++ ShareDB_Server/server.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var express = require('express');
+var WebSocket = require('ws');
+var WebSocketJSONStream = require('websocket-json-stream');
+var ShareDB = require('sharedb');
+require('dotenv').config();
+
+var port = process.env.PORT;
+
+var sharedb = new ShareDB();
+var connection = sharedb.connect();
+connection.get('docs', 'doc1').create({content: 'Type something ...'});
+
+var server = http.createServer();
+var wss = new WebSocket.Server({server: server});
+wss.on('connection', (ws, req) => {
+	var stream = new WebSocketJSONStream(ws);
+	sharedb.listen(stream);
+});
+server.listen(port, () => {
+	console.log(`ShareDB: listening on port ${port}`);
+});
